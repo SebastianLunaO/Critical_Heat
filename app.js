a3593278e4876488deac13af6b6a1b4df77ffd4a
Sebastian Luna <newmsloh@gmail.com>
2025-02-08 21:32:18 -0600

    basic routing and base data for DB

--- app.js
+++ app.js
@@ -9,10 +9,33 @@ app.use(express.json());
 
 
 
-app.get('/', (req, res) => {
-      
+app.get('/api/games', (req, res) => {
+      res.status(200).send({message:"all games"})
 });
 
+app.get('/api/games/:id',(req,res)=>{
+    res.status(200).send({message:"One game"});
+});
+
+app.post('/api/games',(req,res)=>{
+    res.status(201).send({message:"Game added"});
+})
+
+app.put('/api/games/:id',()=>{
+    res.status(200).send({message:"game Edited"});
+})
+
+app.post('/api/reviews',(req,res)=>{
+    res.status(201).send({message:"adding review"});
+})
+
+app.get('/api/reviews',()=>{
+    res.status(200).send({message:"all Reviews"});
+});
+
+app.get('/api/reviews/:id',()=>{
+    res.status(200).send({message:"One Review"});
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
