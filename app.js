63c92f3c2a95ed0af17746e6bd63526e9d0456a5
Sebastian Luna <newmsloh@gmail.com>
2025-02-09 21:16:45 -0600

    Adding new games by the query, first version

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 import cookieParser from 'cookie-parser';
 import express from 'express'
-
+import mysql from 'mysql2/promise'
+import bt from 'bcrypt'
+import z from 'zod'
+import { v4 as uuidv4 } from 'uuid';
 
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 
+const passwordSQL = process.env.PASSWORD_MYSQL
+
+const optionsConnection={
+    host:'localhost',
+    port: 3306,
+    user: 'root',
+    password: passwordSQL,
+    database: 'Critical_Heat'
+}
+
+const db = mysql.createPool(optionsConnection);
 
 
-app.get('/api/games', (req, res) => {
-      res.status(200).send({message:"all games"})
+
+app.get('/api/games', async (req, res) => {
+    const result = await db.query('SELECT * FROM GameTag;') 
+    res.status(200).send(result[0])
 });
 
 app.get('/api/games/:id',(req,res)=>{
     res.status(200).send({message:"One game"});
 });
 
-app.post('/api/games',(req,res)=>{
-    res.status(201).send({message:"Game added"});
+app.post('/api/games',async (req,res)=>{
+    const info = req.body
+    const id = uuidv4();
+    const result = await db.query(`INSERT INTO 
+        Games VALUES 
+        (?,?,?,?,?,?,?,?,?)`,[id,info.title,info.genre,info.developer,
+            info.publisher,info.release_date,info.descp,info.cover_ref,info.Base_price]);
+    res.status(201).send(result[0]);
 })
 
 app.put('/api/games/:id',()=>{
