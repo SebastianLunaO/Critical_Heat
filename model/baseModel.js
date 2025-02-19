a247067f7e05230228253fa7742641a16990a723
Sebastian Luna <newmsloh@gmail.com>
2025-02-19 00:39:32 -0600

    Creating users

--- model/baseModel.js
+++ model/baseModel.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql2/promise'
 import bt from 'bcrypt'
-import z from 'zod'
+import z, { object } from 'zod'
 
 const game = z.object({
     title: z.string(),
@@ -13,6 +13,13 @@ const game = z.object({
     Base_price: z.number(),
     tags: z.string().array()
 });
+
+const user = z.object({
+    username: z.string(),
+    email: z.string().email(),
+    psswd: z.string(),
+    profileP: z.string(),
+})
 
 const passwordSQL = process.env.PASSWORD_MYSQL
 const SALT = process.env.SALT_ROUND
@@ -52,4 +59,44 @@ export class Games {
         return result[0]
     }
 
+    static async getAll(limit=6){
+        const result = await db.query(`SELECT *
+            FROM Games LIMIT ?;`,limit)
+        return result[0]
+    }
+
+}
+
+export class User {
+    static async create(user_info){ 
+        const info = user.parse(user_info)
+
+            const exists = await this.getByName(info.username)
+            if (!(exists===undefined)){
+            throw new Error("Username already exists");
+            return error
+             }
+        
+        const id = crypto.randomUUID();
+        const hashedPassword = bt.hashSync(info.psswd,10);    
+        const result = await db.query(`INSERT INTO 
+        Users(user_id,username,email,passwd,profile_picture_ref) VALUES 
+        (?,?,?,?,?)`,[id,info.username,info.email,hashedPassword,info.profileP]);
+        const aws = await this.getByID(id)
+        return aws
+    }
+
+    static async getByID(id){
+        const result = await db.query (`SELECT user_id,username,profile_picture_ref,join_date 
+            FROM Users WHERE user_id = ?`,id);
+        const row = result[0][0]
+        return row
+    }
+
+    static async getByName(name){
+        const result = await db.query (`SELECT user_id,username,profile_picture_ref,join_date 
+            FROM Users WHERE username = ?`,name);
+        const row = result[0][0]
+        return row
+    }
 }
