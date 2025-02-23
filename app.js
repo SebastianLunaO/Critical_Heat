93f335860dd8e5c8d4ae6b98946401ed968f6ce5
Sebastian Luna <newmsloh@gmail.com>
2025-02-22 23:37:23 -0600

    Add Login user logic

--- app.js
+++ app.js
@@ -103,16 +103,19 @@ app.get('/api/users/:id',async (req,res)=>{
 })
 
 app.get('/login',(req,res)=>{
-    const form = '<h1>Login Page</h1><form method="POST" action="/login">\
-	 Enter Username:<br><input type="text" name="username">\
-	 <br>Enter Password:<br><input type="password" name="password">\
-	 <br><br><input type="submit" value="Submit"></form>';
+    const form = `<h1>Login Page</h1><form method="POST" action="/login">\
+	 Enter Username:<br>
+     <input type="text" name="username">\
+	 <br>Enter Password:<br>
+     <input type="password" name="password">\
+	 <br><br>
+     <input type="submit" value="Submit"></form>`;
  
 	 res.send(form);
 })
 
-app.post('/login',()=>{
-
+app.post('/login',async (req,res)=>{
+    
 })
 
 app.get('/register',(req,res)=>{
